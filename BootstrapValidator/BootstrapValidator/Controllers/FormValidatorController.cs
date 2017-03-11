8176d9b9da2aafaeec04a71fbf6fbb780ec7c834
Leandro Rodrigues <leandro.lacr@yahoo.com.br>
2017-03-11 18:30:07 -0300

    Commit01

---
+++ BootstrapValidator/BootstrapValidator/Controllers/FormValidatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapValidator.Controllers
+{
+    public class FormValidatorController : Controller
+    {
+        // GET: FormValidator
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
